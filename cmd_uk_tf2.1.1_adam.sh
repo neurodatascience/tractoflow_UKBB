3f5d375a40e234ffb6fafedf061e55d94261f20d
Adam Trefonides <adam.trefonides@mcgill.ca>
2020-10-13 18:11:35 -0400

    Formating for clarity

--- cmd_uk_tf2.1.1_adam.sh
+++ cmd_uk_tf2.1.1_adam.sh
@@ -7,7 +7,6 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 #SBATCH --mail-type=ALL
-
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=80G
@@ -20,4 +19,16 @@ module load java/1.8.0_192
 module load nextflow/20.04.1
 module load singularity/3.6
 
-srun nextflow run tractoflow-2.1.1/main.nf --bids ${BIDS_DIR} --dti_shells "0 1000" --fodf_shells "0 1000 2000" -with-singularity tractoflow_2.1.0_feb64b9_2020-05-29.img -resume -with-report report.html --step 0.5 --mean_frf false --set_frf true -profile fully_reproducible -with-mpi --save_seeds false
+srun nextflow run tractoflow-2.1.1/main.nf \
+--bids ${BIDS_DIR} \
+--dti_shells "0 1000" \
+--fodf_shells "0 1000 2000" \
+-with-singularity tractoflow_2.1.0_feb64b9_2020-05-29.img \
+-resume \
+-with-report report.html \
+--step 0.5 \
+--mean_frf false \
+--set_frf true \
+-profile fully_reproducible \
+-with-mpi \
+--save_seeds false
