700d8c444eca58db843dd80e9632dc1880f90a77
Adam Trefonides <adam.trefonides@mcgill.ca>
2021-03-18 15:51:31 -0400

    updated all shell scripts

---
+++ tf_shell_ext3.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#SBATCH --account=rpp-aevans-ab
+#SBATCH --mail-user=adam.trefonides@mcgill.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=1-12:00:00
+
+# --time is set with the assumption that these runs should take 21 hours
+# with some slop
+# This version of the script writes to a loopback mounted ext3 image
+
+ if test $# -lt 1 ; then
+    echo "Usage: $0 [XX] where XX is a 2 to 4 character string"
+    echo "      corresponding to the fake_BIDS directory name"
+     exit 2
+   fi
+
+export FB=$1
+
+# Work directory 
+TASK_ROOT=/lustre03/project/6008063/atrefo/sherbrooke/TF_RUN
+
+# Writable 20G ext3 image file for output
+OUT_IMAGE=${TASK_ROOT}/ext3_images/TF-raw-${FB}.img
+
+# Ouput directory, this is the mounted ext3 image inside the container:
+# The ${FB} directories need to be created before the run, (I think)
+
+OUT_ROOT=/TF_OUT/${FB}
+
+# Current fake BIDS
+BIDS_DIR="$TASK_ROOT/fake_bids/dwi_subs-${FB}"
+
+# Check that the working dirs are there
+cd $TASK_ROOT || exit
+cd $BIDS_DIR || exit
+
+SING_TF_IMAGE=$TASK_ROOT/tractoflow_2.1.0_feb64b9_2020-05-29.img
+
+# UKBB squashfs files
+UKBB_SQUASHFS_DIR=/project/6008063/neurohub/ukbb/imaging
+UKBB_SQUASHFS="
+  neurohub_ukbb_dwi_ses2_0_bids.squashfs
+  neurohub_ukbb_dwi_ses2_1_bids.squashfs
+  neurohub_ukbb_dwi_ses2_2_bids.squashfs
+  neurohub_ukbb_dwi_ses2_3_bids.squashfs
+  neurohub_ukbb_dwi_ses2_4_bids.squashfs
+  neurohub_ukbb_dwi_ses2_5_bids.squashfs
+  neurohub_ukbb_dwi_ses2_6_bids.squashfs
+  neurohub_ukbb_dwi_ses2_7_bids.squashfs
+  neurohub_ukbb_flair_ses2_0_bids.squashfs
+  neurohub_ukbb_rfmri_ses2_0_bids.squashfs
+  neurohub_ukbb_rfmri_ses2_1_bids.squashfs
+  neurohub_ukbb_rfmri_ses2_2_bids.squashfs
+  neurohub_ukbb_rfmri_ses2_3_bids.squashfs
+  neurohub_ukbb_rfmri_ses2_4_bids.squashfs
+  neurohub_ukbb_rfmri_ses2_5_bids.squashfs
+  neurohub_ukbb_rfmri_ses2_6_bids.squashfs
+  neurohub_ukbb_t1_ses2_0_bids.squashfs
+  neurohub_ukbb_t1_ses3_0_bids.squashfs
+  neurohub_ukbb_participants.squashfs
+  neurohub_ukbb_t1_ses2_0_jsonpatch.squashfs
+"
+# Prepared DWI simlink dir with the generated B0 files
+# DWI_SQUASHFS_DIR=/lustre04/scratch/atrefo/sherbrooke/squash_tractoflow
+DWI_SQUASHFS_DIR=${TASK_ROOT}/squash_tractoflow
+DWI_SQUASHFS="
+  dwipipeline.squashfs
+"
+SING_BINDS=" -H ${OUT_ROOT} -B $DWI_SQUASHFS_DIR -B $TASK_ROOT -B ${OUT_IMAGE}:/TF_OUT:image-src=/upper "
+UKBB_OVERLAYS=$(echo "" $UKBB_SQUASHFS | sed -e "s# # --overlay $UKBB_SQUASHFS_DIR/#g")
+DWI_OVERLAYS=$(echo "" $DWI_SQUASHFS | sed -e "s# # --overlay $DWI_SQUASHFS_DIR/#g")
+
+# NOTE: singularity version 3.7.1-1.el7 
+module load singularity/3.7
+
+SINGULARITYENV_NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1) singularity -v shell --cleanenv $SING_BINDS $UKBB_OVERLAYS $DWI_OVERLAYS $SING_TF_IMAGE \
+#  nextflow run /tractoflow/main.nf        \
+#  --bids          ${BIDS_DIR}             \
+#  --output_dir    ${OUT_ROOT}             \
+#  -w              ${OUT_ROOT}/work        \
+#  --dti_shells    "1 1000"                \
+#  --fodf_shells   "0 1000 2000"           \
+#  --step          0.5                     \
+#  --mean_frf      false                   \
+#  --set_frf       true                    \
+#  --save_seeds    false                   \
+#  -profile        fully_reproducible      \
+#  -resume                                 \
+#  -with-report    report.html             \
+#  -with-timeline  timeline.html           \
+#  --processes     4                       \
+#  --processes_brain_extraction_t1 1       \
+#  --processes_denoise_dwi         2       \
+#  --processes_denoise_t1          2       \
+#  --processes_eddy                1       \
+#  --processes_fodf                2       \
+#  --processes_registration        1       \
+
+# previous blank line intentional
+
--- tf_ukbb_bids_prep.sh
+++ tf_ukbb_bids_prep.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-if test $# -lt 2 ; then
-   usage
-   exit 2
-fi
+# This  script is intended to be run from within a singularity shell session that
+# mounts the neurohub UKBB squash images.
+# Once it is finished the resulting symlink farm can be squashed up and overlayed 
+# onto the tractoflow singularity container to run the pipeline.
 
-bidsIN=$1
-bidsOUT=$2
+workDIR="/scratch/atrefo/sherbrooke/symtree"
+bidsIN="/neurohub/ukbb/imaging"
+bidsOUT="${workDIR}/neurohub/ukbb/imaging"
+
+#mkdir -p ${workDIR}
 
 # Create symlinktree
-cp -rs ${bidsIN} ${bidsOUT}
+#echo "Creating symlink tree"
+#cp -rs /neurohub ${workDIR}
 
-# Clean up: remove symlinks for subs that are missing the PA_dwi.json files
+# Clean up subs that are missing the PA_dwi.json files
+# (Once the DTI BIDS is cleaned up this shoudn't be needed)
+echo "removing links to subjects with missing PA_dwi.json files ..."
 for sub in $(find ${bidsOUT} -maxdepth 1 -type d -iname "sub-*" -printf %P"\n" )
 do
 	if [ ! -f "${bidsIN}/${sub}/ses-2/dwi/${sub}_ses-2_acq-PA_dwi.json" ] ; then
 		rm -r ${bidsOUT}/"${sub}"
+		echo "Removed ${sub}"
 	fi
 done
 
 # Main loop
+
 for sub in $(find ${bidsOUT} -maxdepth 1 -type d -iname "sub-*" -printf %P"\n" )
 do
 	echo "working on ${sub}"
@@ -28,6 +36,7 @@ do
 	mkdir -p "${bidsOUT}/${sub}/ses-2/fmap"
 
 	# Do the b0 extraction
+	echo "Doing the b0 extraction on ${sub} ..."
 	scil_extract_b0.py \
 		${bidsOUT}/"${sub}"/ses-2/dwi/"${sub}"_ses-2_acq-PA_dwi.nii.gz \
 		${bidsOUT}/"${sub}"/ses-2/dwi/"${sub}"_ses-2_acq-PA_dwi.bval  \
@@ -41,6 +50,7 @@ do
 	sed -i "s/{/{\n\t\"IntendedFor\":\ \"ses-2\/dwi\/${sub}_ses-2_acq-AP_dwi.nii.gz\",/g"  ${bidsOUT}/"${sub}"/ses-2/fmap/"${sub}"_ses-2_acq-PA_epi.json
 
 	# Remove PA symlinks
-	rm "${sub}"/ses-2/dwi/"${sub}"_ses-2_acq-PA_dwi*
+	rm ${bidsOUT}/"${sub}"/ses-2/dwi/"${sub}"_ses-2_acq-PA_dwi*
+
 	# After this script is run be sure to  run scil_validate_bids.py to make sure it comes up clean.
 done
